a4e280775611a036643f8055a7761215a52aca1f
markziemann <mark.ziemann@gmail.com>
2023-07-14 18:30:08 +1000

    first analysis done

--- ref/ref.sh
+++ ref/ref.sh
@@ -8,4 +8,11 @@ cat Sarcophilus_harrisii.mSarHar1.11.cdna.all.fa.gz Sarcophilus_harrisii.mSarHar
 
 kallisto index -i Sarcophilus_harrisii.mSarHar1.11.cdna+ncrna.fa.gz.idx Sarcophilus_harrisii.mSarHar1.11.cdna+ncrna.fa.gz
 
+# gene name curation
+zcat Sarcophilus_harrisii.mSarHar1.11.cdna+ncrna.fa.gz \
+| grep '>' | sed 's/>//' | sed 's/ /\t/' \
+| sed 's/ gene:/\t/' |  sed 's/gene_symbol:/\t/' \
+| cut -f1,3- | sed 's/ /\t/' | cut -f-2,4- \
+| sed 's/ /\t/' | cut -f-3 | sed 's/$/\t/' \
+| cut -f-3 | sed 's/$/\tNA/' | cut -f-3 > Sarcophilus_harrisii.mSarHar1.11.cdna+ncrna.genenames.tsv
 
