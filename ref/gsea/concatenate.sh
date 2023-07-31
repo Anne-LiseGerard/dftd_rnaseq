6defa570323e1ad407ff3b4d1a5fb6e9fa271e72
Anne-Lise Gérard <anne-lise.gerard@evobio.eu>
2023-07-31 16:09:26 +1000

    corrected concatenate
    previously concatenate.sh inserted duplicate lines in sea.gmt

--- ref/gsea/concatenate.sh
+++ ref/gsea/concatenate.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-find . -name "*.gmt" | while read GMT ;
-do
-  cat "${GMT}" <(echo -e "\r") ;
-done | sed '/^[[:space:]]*$/d' > gsea.gmt
+# Check if there are any .gmt files in the current directory
+shopt -s nullglob
+gmt_files=( *.gmt )
+
+if [ ${#gmt_files[@]} -eq 0 ]; then
+  echo "No .gmt files found in the current directory."
+  exit 1
+fi
+
+# Concatenate .gmt files with a carriage return in between
+output_file="gsea.gmt"
+
+# Remove the output file if it already exists
+if [ -e "$output_file" ]; then
+  rm "$output_file"
+fi
+
+# Loop through each .gmt file and concatenate them
+for gmt_file in "${gmt_files[@]}"; do
+  cat "$gmt_file" >> "$output_file"
+  echo "" >> "$output_file" # Add a carriage return after each file
+done
+
+echo "Concatenation completed. Output saved to $output_file."
 
