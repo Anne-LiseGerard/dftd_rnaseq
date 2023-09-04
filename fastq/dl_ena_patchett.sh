a742d470fbcb2eb8ce436f203b78f85217929537
Anne-Lise Gérard <anne-lise.gerard@evobio.eu>
2023-09-04 16:17:07 +1000

    add tumour biopsy files

---
+++ fastq/dl_ena_patchett.sh
@@ -0,0 +1,8 @@
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR280/006/ERR2804406/ERR2804406.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR280/000/ERR2804410/ERR2804410.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR280/007/ERR2804407/ERR2804407.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR280/009/ERR2804409/ERR2804409.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR280/008/ERR2804408/ERR2804408.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR280/004/ERR2804404/ERR2804404.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR280/003/ERR2804403/ERR2804403.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR280/005/ERR2804405/ERR2804405.fastq.gz
